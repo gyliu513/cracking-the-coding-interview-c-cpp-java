c358668bb797aafa098763254b4c6d0ac4b3cf62
liugya <liugya@liugya-ubuntu.(none)>
2013-10-09 23:54:26 +0800

    Fix bug

--- 19.1/19.1.c
+++ 19.1/19.1.c
@@ -1,19 +1,30 @@
 #include<stdio.h>
-
+ 
 void swap(int *p,int *q)
 {
-	int t;
-	t = *p;
-	*p = *q;
-	*q = t;
+        int t;
+        t = *p;
+        *p = *q;
+        *q = t;
 }
-
+ 
+int swap_opt(int *a, int *b) {
+        *a = *a ^ *b;
+        *b = *a ^ *b;
+        *a = *a ^ *b;
+        return 0;
+}
+ 
 main()
 {
-	int a = 2, b = 3, *p, *q;
-	p = &a;
-	q = &b;
-	swap(p, q);
-	printf("a = %d b = %d\n", a, b);
-	printf("a = %d b = %d\n", *p, *q);
+        int a = 2, b = 3, *p, *q;
+        printf("a = %d b = %d\n", a, b);
+        printf("-------------------\n");
+        p = &a;
+        q = &b;
+        swap(p, q);
+        printf("a = %d b = %d\n", a, b);
+        printf("-------------------\n");
+        swap_opt(&a, &b);
+        printf("a = %d b = %d\n", a, b);
 }
