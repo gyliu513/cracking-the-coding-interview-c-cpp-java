662fd8fa5f030d8b9f6af43d1462947c9e92b985
Guangya Liu <gyliu513@gmail.com>
2013-10-16 21:45:37 -0400

    Linked Lists

---
+++ 2.4/c/2.4c.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct _node
+{
+        struct _node * next;
+        int content;
+} node;
+node *createLinkedList(int list[], int length);
+void printLinkedList(const node *listHead);
+void partitionAtX(node *listHead, int x);
+
+int main()
+{
+        int list[6] = {5, 4, 3, 2, 1, 0};
+        node *head = createLinkedList(list, 6);
+        int x = 3;
+
+        printf("Original:\t");
+        printLinkedList(head);
+        partitionAtX(head, x);
+        printf("Partition:\t");
+        printLinkedList(head);
+
+        return 0;
+}
+
+void partitionAtX(node *listHead, int x)
+{
+        node *head = listHead;
+        node *tmpHead = (node*)malloc(sizeof(node));
+        if(!tmpHead)
+        {
+                printf("Allocate head failed.\n");
+                return;
+        }
+        tmpHead->next = NULL;
+        node *tmpRear = tmpHead;
+        node *prev = listHead;
+        node *p = prev->next;
+        while(p)
+        {
+                if(p->content < x)
+                {
+                        //insert p to the new list
+                        tmpRear->next = p;
+                        tmpRear = p;
+                        //skip p
+                        prev->next = p->next;
+                        p = p->next;
+                }
+                else
+                {
+                        //move on
+                        p = p->next;
+                        prev = prev->next;
+                }
+        }
+        //Link the new list and the old list
+        tmpRear->next = head->next;
+
+        *head = *tmpHead;
+}
+void printLinkedList(const node *listHead)
+{
+        node *p = listHead->next;
+        while(p->next)
+        {
+                printf("%d->", p->content);
+                p = p->next;
+        }
+        printf("%d\n", p->content);
+}
+node *createLinkedList(int list[], int length)
+{
+        node *head = (node*)malloc(sizeof(node));
+        if(head == NULL)
+        {
+                printf("Allocate head failed.\n");
+                return NULL;
+        }
+        head->next = NULL;
+        node *rear = head;
+        //Create the linked list
+        int i, len;
+        for(i=0, len=length; i<len; i++)
+        {
+                node *p = (node*)malloc(sizeof(node));
+                if(p == NULL)
+                {
+                        printf("Allocate head failed.\n");
+                        return NULL;
+                }
+                p->content = list[i];
+                rear->next = p;
+                rear = p;
+        }
+        rear->next = NULL;
+        return head;
+}
