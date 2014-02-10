466632d14a3c2a92fecf1734508b87b8cb4d00fa
gyliu <gyliu@gyliu.com>
2014-02-10 03:01:02 -0500

    update

--- chap4/4.5/cpp/4.5.cpp
+++ chap4/4.5/cpp/4.5.cpp
@@ -1,89 +1,55 @@
 #include <iostream>
 #include <cstring>
-#include <cmath>
-#include <vector>
-#include <list>
 using namespace std;
 
-struct Node {
-	int key;
-	Node *parent, *lchild, *rchild;
+const int maxn = 100;
+struct Node{
+    int key;
+    Node *lchild, *rchild, *parent;
 };
+Node *p, node[maxn];
+int cnt;
 
-Node node[100];
-int cnt = 0;
-
-int createMinimalHeightTree(Node* &head, Node* parent, int a[], int start, int end)
-{
-	if (start <= end) {
-		int mid = (start+end) >> 1;
-		node[cnt].key = a[mid];
-		node[cnt].parent = parent;
-		head = &node[cnt++];
-		createMinimalHeightTree(head->lchild, head, a, start, mid-1);
-		createMinimalHeightTree(head->rchild, head, a, mid+1, end);
-	}
+void init(){
+    p = NULL;
+    memset(node, '\0', sizeof(node));
+    cnt = 0;
 }
-
-int height(Node *head) {
-	if (head == NULL)
-		return 0;
-	return max(height(head->lchild), height(head->rchild)) + 1;
-}
-
-int init()
-{
-	memset(node, '\0', sizeof(node));
-}
-
-vector<list<Node*> > find_level_linklists(Node *head) {
-    vector<list<Node*> > res;
-    list<Node*> li;
-    int level = 0;
-    li.push_back(head);
-    res.push_back(li);
-
-    while (!res[level].empty()) {
-        list<Node *> l;
-        list<Node *>::iterator it;
-        for (it=res[level].begin(); it!=res[level].end(); it++) {
-            Node *n = *it;
-            if (n->lchild) l.push_back(n->lchild);
-            if (n->rchild) l.push_back(n->rchild);
-        }
-        level++;
-        res.push_back(l);
-    }
-    return res;
-}
-
-void print_level(vector<list<Node*> > res) {
-    vector<list<Node*> >::iterator it;
-    for (it=res.begin(); it!=res.end(); it++) {
-        list<Node *> li = *it;
-        list<Node *>::iterator lit;
-        for (lit=li.begin(); lit!=li.end(); lit++) {
-             Node *n = *lit;
-             cout << n->key << " ";
-        }
-        cout << endl;
+void create_minimal_tree(Node* &head, Node *parent, int a[], int start, int end){
+    if(start <= end){
+        int mid = (start + end)>>1;
+        node[cnt].key = a[mid];
+        node[cnt].parent = parent;
+        head = &node[cnt++];
+        create_minimal_tree(head->lchild, head, a, start, mid-1);
+        create_minimal_tree(head->rchild, head, a, mid+1, end);
     }
 }
-
-
-
-int main()
-{
-	//int a[] = {1, 2, 3, 4, 5, 6};
-	int a[] = {
-		0, 1, 2, 3, 4, 5, 6, 7, 8
-	};
-	init();
-	Node *head = NULL;
-	createMinimalHeightTree(head, NULL, a, 0, sizeof(a)/sizeof(a[0]));
-	cout << height(head) << endl;
-        vector<list<Node*> > res;
-        res = find_level_linklists(head);
-        print_level(res);
+Node* minimal(Node* no){    
+    if(no == NULL) return NULL;
+    while(no->lchild)
+        no = no->lchild;
+    return no;
+}
+Node* successor(Node* no){
+    if(no == NULL) return NULL;
+    if(no->rchild) return minimal(no->rchild);
+    Node *y = no->parent;
+    while(y && y->rchild==no){
+        no = y;
+        y = y->parent;
+    }
+    return y;
+}
+int main(){
+    int a[] = {
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    };
+    init();
+    Node *head = NULL;
+    create_minimal_tree(head, NULL, a, 0, 9);
+    cout<<"the head is "<<head->key<<endl;
+    cout<<"the successor of head is: "<<(successor(head))->key<<endl;
+    return 0;
 }
 
